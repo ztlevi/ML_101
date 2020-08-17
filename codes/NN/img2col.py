df2ad82cfbd2aa395f4b5eae69637cb4d0161744
Ting Zhou <ztlevi.work@gmail.com>
2020-08-17 15:56:19 -0700

    add img2col

---
+++ codes/NN/img2col.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def init_layer(img):
+    """
+    Concatenate img features across color channels into (H*W*C, 1). Then normalize.
+    """
+    w, h, c = img.shape
+    layer = np.zeros(shape=(2, 1))
+    for i in range(c):
+        temp = img[:, :, i].reshape(w * h, 1)
+        layer = np.concatenate((layer, temp), axis=0)
+    layer = layer[2:, :]
+    return layer / 255
+
+
+def img2col_with_stride(data_im, channels, height, width, ksize, stride, pad):
+    """
+    Args:
+        data_im: image data in shape (H*W*C, 1).
+        channels: num of input feature depth.
+        height: image height.
+        width: image width.
+        ksize: kernel width and height.
+        stride: convolution stride step.
+        pad: padding length.
+    """
+    height_col = (height + 2 * pad - ksize) // stride + 1
+    width_col = (width + 2 * pad - ksize) // stride + 1
+    channels_col = channels * ksize * ksize
+    data_col = np.zeros(shape=(height_col * width_col * channels_col, 1))
+    for c in range(channels_col):
+        w_offset = np.mod(c, ksize)
+        h_offset = np.mod(np.floor(c // ksize), ksize)
+        c_im = np.floor(np.floor(c // ksize) // ksize)
+        for h in range(height_col):
+            for w in range(width_col):
+                im_row = h_offset + h * stride
+                im_col = w_offset + w * stride
+                col_index = (c * height_col + h) * width_col + w
+                data_col[col_index] = im2col_get_pixel(
+                    data_im, height, width, im_row, im_col, c_im, pad
+                )
+    return data_col
+
+
+def im2col_get_pixel(im, height, width, row, col, channel, pad):
+    """
+    Args:
+        im: input image.
+        height: image height.
+        width: image width.
+        row: row index.
+        col: col index.
+        channel: channel index.
+        pad: padding length.
+    """
+    row = row - pad
+    col = col - pad
+    if row < 0 or col < 0 or row >= height or col >= width:
+        pixel = 0
+    else:
+        pixel = im[int(col + width * (row + height * channel))]
+    return pixel
+
+
+def forward_convolution(
+    layer_in,
+    size_in,
+    kernel_size,
+    maps_in,
+    maps_out,
+    # norm,
+    weights,
+    # biases,
+    # scales,
+    # rolling_mean,
+    # rolling_variance,
+):
+    """
+    Args:
+        layer_in: input feature map in shape (H*W*C, 1).
+        size_in: shape (H, W, C)
+        kernel_size: kernel size.
+        maps_in: input feature map depth.
+        maps_out: output feature map depth.
+    """
+    width = size_in[1]
+    height = size_in[0]
+    size_out = [width, height]
+    stride = 1
+    pad = (kernel_size - 1) / 2
+    data_col = img2col(layer_in, maps_in, height, width, kernel_size, stride, pad)
+    m = maps_out
+    k = kernel_size * kernel_size * maps_in
+    n = width * height
+    A = weights.reshape(m, k)
+    B = data_col.reshape(k, n)
+    C = np.dot(A, B)
+    layer_out = C.reshape(m * n, 1)
+    # if norm:
+    #     for i in range(maps_out):
+    #         for j in range(width * height):
+    #             index = i * width * height + j
+    #             # normalization
+    #             layer_out[index] = (layer_out[index] - rolling_mean[i]) / (
+    #                 np.sqrt(rolling_variance[i]) + 0.000001
+    #             )
+    #             # scale and bias
+    #             layer_out[index] = layer_out[index] * scales[i]
+    #             layer_out[index] = layer_out[index] + biases[i]
+    #             # activation
+    #             if layer_out[index] < 0:
+    #                 layer_out[index] = layer_out[index] * 0.1
+    # else:
+    #     for i in range(maps_out):
+    #         for j in range(width * height):
+    #             index = i * width * height + j
+    #             layer_out[index] = layer_out[index] + biases[i]
+    print(
+        "Done! %d*%d*%d=>%d*%d*%d" % (width, height, maps_in, width, height, maps_out)
+    )
+    return layer_out, size_out
+
+
+img = np.zeros((5, 5, 3))
+data_im = init_layer(img)
+img2col_with_stride(data_im, 3, 5, 5, 3, 1, 0)
---
+++ codes/NN/img2col_cs231_version.py
@@ -0,0 +1,88 @@
+from builtins import range
+
+import numpy as np
+
+
+def get_im2col_indices(x_shape, field_height, field_width, padding=1, stride=1):
+    # First figure out what the size of the output should be
+    N, C, H, W = x_shape
+    assert (H + 2 * padding - field_height) % stride == 0
+    assert (W + 2 * padding - field_height) % stride == 0
+    out_height = (H + 2 * padding - field_height) // stride + 1
+    out_width = (W + 2 * padding - field_width) // stride + 1
+
+    i0 = np.repeat(np.arange(field_height), field_width)
+    i0 = np.tile(i0, C)
+    i1 = stride * np.repeat(np.arange(out_height), out_width)
+    j0 = np.tile(np.arange(field_width), field_height * C)
+    j1 = stride * np.tile(np.arange(out_width), out_height)
+    i = i0.reshape(-1, 1) + i1.reshape(1, -1)
+    j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+
+    k = np.repeat(np.arange(C), field_height * field_width).reshape(-1, 1)
+
+    return k, i, j
+
+
+def im2col_indices(x, field_height, field_width, padding=1, stride=1):
+    """ An implementation of im2col based on some fancy indexing """
+    # Zero-pad the input
+    p = padding
+    x_padded = np.pad(x, ((0, 0), (0, 0), (p, p), (p, p)), mode="constant")
+
+    k, i, j = get_im2col_indices(x.shape, field_height, field_width, padding, stride)
+
+    cols = x_padded[:, k, i, j]
+    C = x.shape[1]
+    cols = cols.transpose(1, 2, 0).reshape(field_height * field_width * C, -1)
+    return cols
+
+
+def col2im_indices(cols, x_shape, field_height=3, field_width=3, padding=1, stride=1):
+    """ An implementation of col2im based on fancy indexing and np.add.at """
+    N, C, H, W = x_shape
+    H_padded, W_padded = H + 2 * padding, W + 2 * padding
+    x_padded = np.zeros((N, C, H_padded, W_padded), dtype=cols.dtype)
+    k, i, j = get_im2col_indices(x_shape, field_height, field_width, padding, stride)
+    cols_reshaped = cols.reshape(C * field_height * field_width, -1, N)
+    cols_reshaped = cols_reshaped.transpose(2, 0, 1)
+    np.add.at(x_padded, (slice(None), k, i, j), cols_reshaped)
+    if padding == 0:
+        return x_padded
+    return x_padded[:, :, padding:-padding, padding:-padding]
+
+
+def conv_forward_im2col(x, w, b, conv_param):
+    """
+    A fast implementation of the forward pass for a convolutional layer
+    based on im2col and col2im.
+    """
+    N, C, H, W = x.shape
+    num_filters, _, filter_height, filter_width = w.shape
+    stride, pad = conv_param["stride"], conv_param["pad"]
+
+    # Check dimensions
+    assert (W + 2 * pad - filter_width) % stride == 0, "width does not work"
+    assert (H + 2 * pad - filter_height) % stride == 0, "height does not work"
+
+    # Create output
+    out_height = (H + 2 * pad - filter_height) // stride + 1
+    out_width = (W + 2 * pad - filter_width) // stride + 1
+    out = np.zeros((N, num_filters, out_height, out_width), dtype=x.dtype)
+
+    x_cols = im2col_indices(x, w.shape[2], w.shape[3], pad, stride)
+    # x_cols = im2col_cython(x, w.shape[2], w.shape[3], pad, stride)
+    res = w.reshape((w.shape[0], -1)).dot(x_cols) + b.reshape(-1, 1)
+
+    out = res.reshape(w.shape[0], out.shape[2], out.shape[3], x.shape[0])
+    out = out.transpose(3, 0, 1, 2)
+
+    cache = (x, w, b, conv_param, x_cols)
+    return out, cache
+
+
+img = np.zeros((1, 3, 5, 5))
+# kernel size 3x3
+# conv output size (field_height, field_width)
+field_height, field_width = 3, 3
+im2col_indices(img, field_height, field_width)
