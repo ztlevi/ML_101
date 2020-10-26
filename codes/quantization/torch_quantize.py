fix softmax

codes/quantization/torch_quantize.py
@@ -33,17 +33,25 @@ float_tensor = torch.randn(2, 2, 3)
 
 scale, zero_point = 0.01, 2
 dtype = torch.qint8
-float_tensorq_per_tensor = torch.quantize_per_tensor(
-    float_tensor, scale, zero_point, dtype
-).int_repr()
+float_tensorq_per_tensor = torch.quantize_per_tensor(float_tensor, scale, zero_point, dtype).int_repr()
 print("float_tensor: ", float_tensor)
+# >>> float_tensor:  tensor([[[-0.0687,  1.1466, -0.0254],
+#          [-0.9382, -2.5390,  0.7466]],
+#         [[ 0.2182,  0.8028,  0.2443],
+#          [-1.0790, -0.7884, -0.1219]]])
 print("float_tensorq_per_tensor: ", float_tensorq_per_tensor)
+# >>> float_tensorq_per_tensor:  tensor([[[  -5,  117,   -1],
+#          [ -92, -128,   77]],
+#         [[  24,   82,   26],
+#          [-106,  -77,  -10]]], dtype=torch.int8)
 print()
 
 float_tensor = torch.tensor([-1.0, 0.0, 1.0, 2.0])
 int_tensor = torch.quantize_per_tensor(float_tensor, 0.1, 10, torch.quint8).int_repr()
 print("float_tensor: ", float_tensor)
+# >>> float_tensor:  tensor([-1.,  0.,  1.,  2.])
 print("int_tensor: ", int_tensor)
+# >>> int_tensor:  tensor([ 0, 10, 20, 30], dtype=torch.uint8)
 print()
 
 # Help on built-in function quantize_per_channel:
@@ -82,20 +90,27 @@ scales = torch.tensor([1e-1, 1e-2, 1e-3])
 zero_points = torch.tensor([-1, 0, 1])
 channel_axis = 2
 dtype = torch.qint32
-q_per_channel = torch.quantize_per_channel(
-    float_tensor, scales, zero_points, axis=channel_axis, dtype=dtype
-).int_repr()
+q_per_channel = torch.quantize_per_channel(float_tensor, scales, zero_points, axis=channel_axis, dtype=dtype).int_repr()
 print("float_tensor: ", float_tensor)
+# >>> float_tensor:  tensor([[[ 0.7369,  1.0839, -0.9761],
+#          [-2.2165, -0.1486, -0.6503]],
+#         [[-0.5460,  0.1936, -0.7754],
+#          [-0.9335, -0.2668, -0.0625]]])
 print("q_per_channel: ", q_per_channel)
+# >>> q_per_channel:  tensor([[[   6,  108, -975],
+#          [ -23,  -15, -649]],
+#         [[  -6,   19, -774],
+#          [ -10,  -27,  -61]]], dtype=torch.int32)
 print()
 
 # 2. Create a quantized Tensor directly from empty_quantized functions
 # Note that _empty_affine_quantized is a private API, we will replace it
 # something like torch.empty_quantized_tensor(sizes, quantizer) in the future
-q = torch._empty_affine_quantized(
-    [10], scale=scale, zero_point=zero_point, dtype=dtype
-).int_repr()
+q = torch._empty_affine_quantized([10], scale=scale, zero_point=zero_point, dtype=dtype).int_repr()
 print(q)
+
+# >>> tensor([2053701646,        101,          0,          0,          0,          0,
+#           60826480,          1, 1293628728,      32653], dtype=torch.int32)
 
 # 3. Create a quantized Tensor by assembling int Tensors and quantization parameters
 # Note that _per_tensor_affine_qtensor is a private API, we will replace it with
@@ -108,8 +123,7 @@ int_tensor = torch.randint(0, 100, size=(10,), dtype=torch.uint8)
 # - torch.uint8 -> torch.quint8
 # - torch.int8 -> torch.qint8
 # - torch.int32 -> torch.qint32
-q = torch._make_per_tensor_quantized_tensor(
-    int_tensor, scale, zero_point
-).int_repr()  # Note no `dtype`
+q = torch._make_per_tensor_quantized_tensor(int_tensor, scale, zero_point).int_repr()  # Note no `dtype`
 
 print(q)
+# >>> tensor([37, 89, 98, 47, 58, 52, 81, 62, 55, 86], dtype=torch.uint8)

codes/softmax_loss/softmax_loss.py
@@ -10,7 +10,7 @@ from torch.autograd import Function, Variable
 
 def softmax(z):
     z -= np.max(z)
-    sm = (np.exp(z).T / np.sum(np.exp(z), axis=0)).T
+    sm = np.exp(z) / np.expand_dims(np.sum(np.exp(z), axis=1), axis=1)
     return sm
 
 
@@ -107,9 +107,7 @@ def test_softmax_loss_backward():
     x_var = Variable(x, requires_grad=True)
 
     # testing labels
-    target = torch.LongTensor(
-        np.repeat(np.array(range(num_classes)), x_size // num_classes)
-    )
+    target = torch.LongTensor(np.repeat(np.array(range(num_classes)), x_size // num_classes))
     target_var = Variable(target)
 
     # compute outputs of softmax loss